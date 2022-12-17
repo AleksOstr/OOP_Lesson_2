d3765c5a6211fa37be766b5052e3205ad84f5099
AleksOstr <ostryankoam@yandex.ru>
2022-12-17 11:58:48 +0400

    Add Animal class

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,32 @@
+import Interfaces.Runable;
+import Interfaces.Speakable;
+
 public class Animal {
+    String nickname;
+    int legs;
+    String owner;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public Animal(String name, int legs, String owner) {
+        this.nickname = name;
+        this.legs = legs;
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Animal: %s Nickname: %s Owner: %s Legs: %d",
+                            this.getClass(), this.getNickname(), this.getOwner(), this.getLegs());
+    }
 }
