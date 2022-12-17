47a00e6148043c9a4641baf1ccd0f86a448ede05
AleksOstr <ostryankoam@yandex.ru>
2022-12-17 12:13:26 +0400

    Add Dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,23 @@
-public class Dog {
+import Interfaces.Runable;
+import Interfaces.Speakable;
+
+public class Dog extends Animal implements Speakable, Runable {
+    public Dog(String name, int legs, String owner) {
+        super(name, legs, owner);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog " + super.toString();
+    }
+
+    @Override
+    public int runSpeed() {
+        return 45;
+    }
+
+    @Override
+    public String speak() {
+        return "Woof woof";
+    }
 }
